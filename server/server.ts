fa6c951669d3e7313707932aff2473688b4dfcb5
Harry177 <harry177@rambler.ru>
2023-08-14 21:10:03 +0200

    refactor: remove local https settings due to independent heroku https settings

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,4 @@
 import express from 'express';
-import https from 'https';
-import fs from 'fs';
 import path from 'path';
 
 const app = express();
@@ -9,20 +7,12 @@ const port = 3000;
 
 const clientApp = path.join(__dirname, '../build');
 
-const privateKeyPath = path.join(__dirname, '../server.key');
-const certificatePath = path.join(__dirname, '../server.crt');
-
 app.use(express.static(clientApp));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(clientApp, 'index.html'));
 });
 
-const serverOptions = {
-  key: fs.readFileSync(privateKeyPath),
-  cert: fs.readFileSync(certificatePath),
-};
-
-https.createServer(serverOptions, app).listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
