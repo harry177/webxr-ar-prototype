6dab059cb6ef859c9b60b7f27260b71d9856f152
Harry177 <harry177@rambler.ru>
2023-08-16 18:08:44 +0200

    feat: implement videotexture for cube

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -10,7 +10,8 @@ export const Scene: React.FC = () => {
   const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
   const cubeRef = useRef<THREE.Mesh | null>(null);
   const raycasterRef = useRef<THREE.Raycaster | null>(null);
-  //const readyRef = useRef<boolean>(false);
+  const textureRef = useRef<THREE.VideoTexture | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
     const init = () => {
@@ -38,32 +39,39 @@ export const Scene: React.FC = () => {
       const arButton = ARButton.createButton(renderer);
       arButton.addEventListener("click", handleARSession);
 
-      //const session = renderer.xr.getSession();
-      //readyRef.current = renderer.xr.isPresenting;
+      const video = videoRef.current;
+     
+      if (video) {
+        video.play();
+        const texture = new THREE.VideoTexture(video);
+        texture.minFilter = THREE.LinearFilter;
+        texture.magFilter = THREE.LinearFilter;
+        texture.format = THREE.RGBAFormat;
 
-      const loader = new THREE.TextureLoader();
-      const texture = loader.load(require("../assets/tv-texture.jpg"));
-      texture.colorSpace = THREE.SRGBColorSpace;
+        textureRef.current = texture;
 
-      const geometry = new THREE.BoxGeometry();
-      const material = new THREE.MeshBasicMaterial({ map: texture });
-      const cube = new THREE.Mesh(geometry, material);
-      cube.scale.set(0.5, 0.5, 0.5);
-      cube.position.set(0, 0, -2.5);
-      cubeRef.current = cube;
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({
+          map: texture,
+          side: THREE.FrontSide,
+          toneMapped: false,
+        });
+        const cube = new THREE.Mesh(geometry, material);
+        cube.scale.set(0.5, 0.5, 0.5);
+        cube.position.set(0, 0, -2.5);
+        cubeRef.current = cube;
 
-      //scene.add(cube);
+        renderer.setAnimationLoop(animate);
 
-      renderer.setAnimationLoop(animate);
+        document.body.appendChild(arButton);
 
-      document.body.appendChild(arButton);
+        container.appendChild(renderer.domElement);
 
-      container.appendChild(renderer.domElement);
+        camera.position.z = 5;
 
-      camera.position.z = 5;
-
-      const raycaster = new THREE.Raycaster();
-      raycasterRef.current = raycaster;
+        const raycaster = new THREE.Raycaster();
+        raycasterRef.current = raycaster;
+      }
     };
 
     const animate = () => {
@@ -71,11 +79,16 @@ export const Scene: React.FC = () => {
       const { current: scene } = sceneRef;
       const { current: camera } = cameraRef;
       const { current: cube } = cubeRef;
+      const { current: texture } = textureRef;
 
       if (renderer && scene && camera) {
         if (cube) {
           cube.rotation.x += 0.01;
           cube.rotation.y += 0.01;
+        }
+
+        if (texture) {
+          texture.needsUpdate = true;
         }
 
         renderer.render(scene, camera);
@@ -103,6 +116,21 @@ export const Scene: React.FC = () => {
 
   return (
     <>
+      <video
+        ref={videoRef}
+        className="video-texture"
+        width="100%"
+        height="100%"
+        playsInline
+        muted
+        autoPlay
+        loop
+        preload="auto"
+        crossOrigin="anonymous"
+        controls
+      >
+        <source type="video/mp4" src="https://webglsamples.org/color-adjust/sample-video.mp4"></source>
+      </video>
       <div ref={containerRef} className="scene-container" />
     </>
   );
